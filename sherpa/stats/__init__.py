695a10672abb2275896316518c716b0b90df6e1c
Dan Nguyen <dnguyen@cfa.harvard.edu>
2018-04-11 13:36:41 -0400

    Centralize the eval_model_to_fit for simultaneous data
    (Wstat was calling the Data-specific
    class eval_model_to_fit method which will wreak havoc for later update)

--- sherpa/stats/__init__.py
+++ sherpa/stats/__init__.py
@@ -1,5 +1,5 @@
 #
-#  Copyright (C) 2009, 2015, 2016, 2017  Smithsonian Astrophysical Observatory
+#  Copyright (C) 2009, 2015, 2016, 2017, 2018 Smithsonian Astrophysical Observatory
 #
 #
 #  This program is free software; you can redistribute it and/or modify
@@ -830,7 +830,7 @@ class WStat(Likelihood):
         # original code used this approach.
         #
         data_src = []
-        data_model = []
+        data_model = data.eval_model_to_fit(model)
         data_bkg = []
         nelems = []
         exp_src = []
@@ -841,7 +841,6 @@ class WStat(Likelihood):
 
             y = dset.to_fit(staterrfunc=None)[0]
             data_src.append(y)
-            data_model.append(dset.eval_model_to_fit(mexpr))
             nelems.append(y.size)
 
             try:
@@ -919,7 +918,6 @@ class WStat(Likelihood):
             exp_bkg.append(bset.exposure * ascal)
 
         data_src = numpy.concatenate(data_src)
-        data_model = numpy.concatenate(data_model)
         exp_src = numpy.concatenate(exp_src)
         exp_bkg = numpy.concatenate(exp_bkg)
         data_bkg = numpy.concatenate(data_bkg)
